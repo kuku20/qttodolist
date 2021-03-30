64499d6e07409d168d88165bfd0e61611b58cb44
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-03-29 23:27:58 -0500

    add comment

--- create_todolist_dialog.cpp
+++ create_todolist_dialog.cpp
@@ -26,25 +26,30 @@ QString create_todolist_dialog::getdate(){
     QString temp = get_date;
     return temp;
 }
+//create new to-dolist
 void create_todolist_dialog::on_pushButton_clicked()
 {
     int exist_u = 0;
     QString list_name = ui->lineEdit->text();
     queryOption queryOption;
     exist_u=queryOption.checkIfExist(list_name,"catalog");
+    //check if this name cata already exist
     if(exist_u<0){
         QMessageBox::warning(this, "Warning",
                              "This  catalog already exist!!!");
     }
+    //check if user not input any
     else if( list_name==""){
         QMessageBox::warning(this, "Warning",
                              "Please input name catalog !!!");
     }
+    //check if the forgot to choose the day
     else if(getdate()==""){
         QMessageBox::warning(this, "Warning",
                              "Please choose date!!!");
     }
     else{
+        //go to create
         queryOption.newList(list_name, getdate());
         QMessageBox::information(this,"Create catalog",
                                  "the " +list_name+" created.");
@@ -52,7 +57,7 @@ void create_todolist_dialog::on_pushButton_clicked()
 
     }
 }
-
+//click to the calendar and get the date
 void create_todolist_dialog::on_calendarWidget_clicked(const QDate &date)
 {
     QString date_input= date.toString("yyyy-MM-dd");
