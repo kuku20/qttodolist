def8612738dfbdfa747c7870b9ca48971148c938
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-04-24 16:47:26 -0500

    fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,10 +12,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     this->setWindowTitle("Wellcom to TO_DO_LIST");
-        //call the database
-        queryOption queryOption;
-        queryOption::setCon();
-       queryOption.createUser();
+    queryOption queryOption;
+    queryOption::setCon();
+    queryOption.createUser();
+
 }
 
 MainWindow::~MainWindow()
@@ -23,8 +23,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//user click the login button
 
+//user click the login button
 void MainWindow::on_pushButton_clicked()
 {
     QString username = ui->lineEdit_userInput->text();
@@ -34,30 +34,28 @@ void MainWindow::on_pushButton_clicked()
     bool userlogined=false;
     query.exec("SELECT * FROM users");
     while (query.next()) {
-            QString usernamedb = query.value(2).toString();
-            QString passdb = query.value(1).toString();
-        //compare to the database
-            if(username==usernamedb && password==passdb){
-                QMessageBox::information(this, "Login", "You are login!!");
-                userlogined=true;
-                close();
-                //set the username and user Id
-                queryOption::accessID(query.value(3).toString());
-                queryOption::accessUser(username);
-                secondMain secondMain;
-                //open the second dialog
-                queryOption::setCon(db);
-                secondMain.setModal(true);
-                secondMain.exec();
-                break;
-            }
+        QString usernamedb = query.value(2).toString();
+        QString passdb = query.value(1).toString();
+        if(username==usernamedb && password==passdb){
+            QMessageBox::information(this, "Login", "You are login!!");
+            userlogined=true;
+            close();
+            queryOption::accessID(query.value(3).toString());
+            queryOption::accessUser(username);
+            secondMain secondMain;
+            //                secondMain.userreturn(username,query.value(3).toString());
+            queryOption::setCon(db);
+            secondMain.setModal(true);
+            secondMain.exec();
+
+            break;
         }
+    }
     if(userlogined==false){
         QMessageBox::warning(this,"Login","Username or password is not correct");
 
     }
 }
-
 
 /*
 This function to give user to create new account
@@ -73,6 +71,7 @@ void MainWindow::on_pushButton_3_clicked()
     //check if the user or email exist in the database
     exist_u=queryOption.checkIfExist(new_user,"users");
     exist_e=queryOption.checkIfExist(new_email,"users");
+    QRegularExpression regex("^[0-9a-zA-Z]+([0-9a-zA-Z]*[-._+])*[0-9a-zA-Z]+@[0-9a-zA-Z]+([-.][0-9a-zA-Z]+)*([0-9a-zA-Z]*[.])[a-zA-Z]{2,6}$");
     if(exist_u<0 or exist_e <0){
         if(exist_u<0){
             QMessageBox::information(this, "Warning",
@@ -82,21 +81,28 @@ void MainWindow::on_pushButton_3_clicked()
             QMessageBox::information(this, "Warning",
                                      "This  Email already exist!!!");
         }
-        }
+    }
+    else if(!regex.match(new_email).hasMatch()){
+        QMessageBox::information(this, "Warning",
+                                 "Email wrong formmat!!!");
+    }
     else
     {
-        //add to the database
+
+       //add to the database
         QSqlDatabase db = QSqlDatabase::database();
         QSqlQuery query(db);
         query.prepare("INSERT INTO users(username, email, password) "
-                          "VALUES (:username, :email, :password)");
+                      "VALUES (:username, :email, :password)");
         query.bindValue(":username", new_user);
         query.bindValue(":email", new_email);
         query.bindValue(":password", new_pass);
-        if(query.exec()) {
+        if(query.exec()){
             QMessageBox::information(this, "New account created", "You have create account!!");
+        }else{
+            QMessageBox::information(this, "Not connet", "not connet to database!!");
         }
-        else
-            qDebug() << "ERROR: " << query.lastError().text();
+        ;
+
     }
 }
